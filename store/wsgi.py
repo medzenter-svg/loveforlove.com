37dcbc5db8bd63c06b3b6575d5b2ffb682bdfb7b
medzenter-svg <medzenter@icloud.com>
2026-08-20 15:40:41 +0200

    Install persistent paid-order access in production app

--- store/wsgi.py
+++ store/wsgi.py
@@ -1,6 +1,10 @@
-from app import app
+import app as store_app
 from payments import payments_bp
+from persistent_access import install_persistent_paid_access
 
+
+install_persistent_paid_access(store_app)
+app = store_app.app
 
 if "payments" not in app.blueprints:
     app.register_blueprint(payments_bp)
